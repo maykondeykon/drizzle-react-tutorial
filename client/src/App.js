b010a94ebc9259d247073a1fc4a6655f7c2a252d
Maykon Deykon <maykondeykon@gmail.com>
2019-02-25 20:38:54 -0300

    Adding ReadString component

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import ReadString from './ReadString';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {loading: true, drizzleState: null};
+
+    componentDidMount() {
+        const {drizzle} = this.props;
+        this.unsubscribe = drizzle.store.subscribe(() => {
+            const drizzleState = drizzle.store.getState();
+
+            if (drizzleState.drizzleStatus.initialized) {
+                this.setState({loading: false, drizzleState});
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    render() {
+        if (this.state.loading) return "Loading Drizzle...";
+        return (<div className="App">
+            <ReadString
+                drizzle={this.props.drizzle}
+                drizzleState={this.state.drizzleState}
+            />
+        </div>);
+    }
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Drizzle, generateStore} from "drizzle";
+import MyStringStore from "./contracts/MyStringStore.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const options = {contracts: [MyStringStore]};
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(<App drizzle={ drizzle }/>, document.getElementById('root'));
 serviceWorker.unregister();
